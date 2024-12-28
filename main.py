2eb0eb854ff9b8f55026b1d1f7e020163d5fd3ab
Pinheiro <mateuspinheirodev@gmail.com>
2024-12-28 01:05:51 -0300

    Creating frames and structure of layout

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 from tkinter import *
 from tkinter import ttk
 
-janela = Tk()
-janela.title("Calculadora")
+colordsp = "#798686" #display
+colorbg = "#e6f2ff" #bg
+colorbody = "#798686" #body
 
+window = Tk()
+window.title("Calculadora")
+window.geometry("235x318")
+window.config(bg=colorbg)
+
+#frames
+frame_display = Frame(window, width=235, height=70, bg=colordsp)
+frame_display.grid(row=0, column=0)
+
+frame_body = Frame(window, width=235, height=258, bg=colorbg)
+frame_body.grid(row=1, column=0)
+
+#buttons
+
+
+window.mainloop()
